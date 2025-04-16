6873e43ab73e3aecd9849b65d93dbd8eea78dad7
Auberon <auberon.lopez@greenriver.edu>
2025-04-16 11:33:16 -0700

    Revert "Fixed bug in describePasswordLength and added tests"
    This reverts commit 4fbc2cfe114e5a56d068d1f0b160cd36a9a1a6fa.

--- src/PasswordUtilsTest.java
+++ src/PasswordUtilsTest.java
@@ -4,31 +4,6 @@ import static org.junit.jupiter.api.Assertions.*;
 public class PasswordUtilsTest {
     @Test
     void testDescribePasswordLengthShortPassword() {
-        // Arrange
-        String password = "hey";
-        // Act
-        String actual = PasswordUtils.describePasswordLength(password);
-        // Assert
-        assertEquals("short", actual);
-    }
 
-    @Test
-    void testDescribePasswordLengthExactly12Chars() {
-        // Arrange
-        String password = "helloSDEV220";
-        // Act
-        String actual = PasswordUtils.describePasswordLength(password);
-        // Assert
-        assertEquals("long", actual);
-    }
-
-    @Test
-    void testDescribePasswordLengthMedium() {
-        // Arrange
-        String password = "helloSDEV";
-        // Act
-        String actual = PasswordUtils.describePasswordLength(password);
-        // Assert
-        assertEquals("medium", actual);
     }
 }
