c8a14182870802477aff5b665477eb404754dc41
Brendan Villaraza <villaraza.brendan@student.greenriver.edu>
2025-04-16 11:27:12 -0700

    Fixed bug in describedPasswordLength and added tests

--- src/PasswordUtils.java
+++ src/PasswordUtils.java
@@ -2,6 +2,11 @@
  * Utility class for analyzing passwords.
  */
 public class PasswordUtils {
+    public static void main(String[] args){
+        // String result  = describePasswordLength("helloSDEV220");
+        // System.out.println(result);
+
+    }
 
     /**
      * Returns a description of the password's length.
@@ -15,7 +20,7 @@ public class PasswordUtils {
         int length = password.length();
         if (length < 6) {
             return "short";
-        } else if (length <= 12) {
+        } else if (length < 12) {
             return "medium";
         }
         return "long";
--- src/PasswordUtilsTest.java
+++ src/PasswordUtilsTest.java
@@ -4,6 +4,32 @@ import static org.junit.jupiter.api.Assertions.*;
 public class PasswordUtilsTest {
     @Test
     void testDescribePasswordLengthShortPassword() {
+        // Arrange
+        String password = "hey";
 
+
+        // Act
+        String actual = PasswordUtils.describePasswordLength(password);
+
+        // Assert
+        assertEquals("short", actual);
+    }
+    @Test
+    void testDescribePasswordLengthExactly12Chars(){
+        //Arrange
+        String password = "helloSDEV220";
+        //Act
+        String actual = PasswordUtils.describePasswordLength(password);
+        //assert
+        assertEquals("long", actual);
+    }
+    @Test
+    void testDescribePasswordLengthMedium(){
+        //Arrange
+        String password = "helloSDEV";
+        //Act
+        String actual = PasswordUtils.describePasswordLength(password);
+        //assert
+        assertEquals("medium", actual);
     }
 }
