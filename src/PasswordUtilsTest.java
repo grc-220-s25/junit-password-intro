21e5ac80de1c81e2a49dc9794d8a6cd5f72948bd
kriseattle <notbbbfamous@gmail.com>
2025-04-16 11:26:37 -0700

    Fixed bug in describePasswordLength and added tests

--- src/PasswordUtilsTest.java
+++ src/PasswordUtilsTest.java
@@ -5,5 +5,52 @@ public class PasswordUtilsTest {
     @Test
     void testDescribePasswordLengthShortPassword() {
 
+        // Arrange
+        String password = "hey";
+
+
+        // Act
+        String actual = PasswordUtils.describePasswordLength(password);
+
+        // We dont have to manually add print method, junit will do it for us under Assert part
+        //System.out.println(actual);
+
+
+        // Assert
+        // comparing 2 variables for the test, where one is expect, and one is actual one
+        assertEquals("short", actual);
+
     }
+
+    @Test
+    void testDescribePasswordLengthExactly12chars(){
+
+        // Arrange
+        String password = "hellosdev220";
+
+        // Act
+        String actual = PasswordUtils.describePasswordLength(password);
+
+        // Assert
+        assertEquals("long", actual);
+    }
+
+
+    @Test
+    void testDescribePasswordLengthMedium(){
+
+        // Arrange
+        String password = "helloSDEV";
+
+        // Act
+        String actual = PasswordUtils.describePasswordLength(password);
+
+        // Assert
+        assertEquals("medium", actual);
+    }
+
+
+
+
+    
 }
