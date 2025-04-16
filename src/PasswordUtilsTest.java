a9c6b2ae05b54ce2497b675e6d12fd78e57481ec
Kiyum-06 <kiyum1406@gmail.com>
2025-04-16 11:27:27 -0700

    fixed buged in describePasswordLength and added tests

--- src/PasswordUtilsTest.java
+++ src/PasswordUtilsTest.java
@@ -4,6 +4,36 @@ import static org.junit.jupiter.api.Assertions.*;
 public class PasswordUtilsTest {
     @Test
     void testDescribePasswordLengthShortPassword() {
+        // Arrange
+        String password = "hey"; 
+        // Act
+        String actual = PasswordUtils.describePasswordLength(password);
+        //Assert
+        assertEquals("short", actual);
 
     }
+
+    @Test
+    void testDescribePasswordLengthExactly12Chars() {
+        //Arrange
+        String password = "helloSDEV220";
+        //Act
+        String actual = PasswordUtils.describePasswordLength(password);
+        //Assert
+        assetEquals("long", actual);
+
+    }
+
+    @Test
+    void testDescribePasswordLengthMedium() {
+        //Arrange
+        String password = "helloSDEV";
+        //Act
+        String actual = PasswordUtils.describePasswordLength(password);
+        //Assert
+        assetEquals("medium", actual);
+
+    }
+    
+
 }
