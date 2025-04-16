a635e057aac7c11f3e2cf1b31e3f6db920abe047
Johncarlo Perez <johncarloperez@Johncarlos-MacBook-Pro.local>
2025-04-16 12:20:59 -0700

    Fixed bug in isAlphanumeric method and removed unnesscary return statement

--- src/PasswordUtilsTest.java
+++ src/PasswordUtilsTest.java
@@ -30,4 +30,31 @@ public class PasswordUtilsTest {
         // assert
         assertEquals("medium", actual);
     }
+    @Test
+    void checkingForAlphanumeric() {
+        // arrange
+        String password = "helloSDEV220";
+        // act
+        boolean actual = PasswordUtils.isAlphanumeric(password);
+        // assert
+        assertEquals(true, actual);
+    }
+    @Test
+    void checkingForNumbers() {
+        // arrange
+        String password = "12345678";
+        // act
+        boolean actual = PasswordUtils.isAlphanumeric(password);
+        // assert
+        assertEquals(true, actual);
+    }
+    @Test
+    void checkingIfHasSpecialChar() {
+        // arrange
+        String password = "123456$ 2";
+        // act
+        boolean actual = PasswordUtils.isAlphanumeric(password);
+        // assert
+        assertEquals(false, actual);
+    }
 }
