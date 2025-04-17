8329fc011bd7196692dcc4f5782413be77e88383
Gulyapas Poonkawinsiri <67386394+joetlobb@users.noreply.github.com>
2025-04-17 09:37:19 -0700

    add test for describePasswordLength and fix the bug

--- src/PasswordUtilsTest.java
+++ src/PasswordUtilsTest.java
@@ -4,6 +4,37 @@ import static org.junit.jupiter.api.Assertions.*;
 public class PasswordUtilsTest {
     @Test
     void testDescribePasswordLengthShortPassword() {
+        // Arrange
+        String password = "hey";
 
+        // Act
+        String actual = PasswordUtils.describePasswordLength(password);
+
+        // Assert: the result SHOULD be 'short'. Compare it to actual result.
+        assertEquals("short", actual);
+    }
+
+    @Test
+    void testDescribePasswordLengthLongPassword() {
+        // Arrange
+        String password = "helloSDEV220";
+
+        // Act
+        String actual = PasswordUtils.describePasswordLength(password); 
+
+        // Assert
+        assertEquals("long", actual);
+    }
+
+    @Test
+    void testDescribePasswordLengthMediumPassword() {
+        // Arrange
+        String password = "helloSDEV";
+
+        // Act
+        String actual = PasswordUtils.describePasswordLength(password); 
+
+        // Assert
+        assertEquals("medium", actual);
     }
 }
